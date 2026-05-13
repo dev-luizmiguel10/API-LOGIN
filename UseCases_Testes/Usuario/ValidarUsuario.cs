5cb1097695ffcaec62b5a44c543deb521ea7b052
Luiz Aguiar <mluiz8650@gmail.com>
2026-05-12 21:57:39 -0300

    teste unitario com bogus , lib com dados fake

---
+++ UseCases_Testes/Usuario/ValidarUsuario.cs
@@ -0,0 +1,21 @@
+﻿using ApiLogin.Comunication.Request;
+using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseCases_Testes.Usuario
+{
+    public class ValidarUsuario
+    {
+        public static UsuarioRequest validate ()
+        {
+            return new Faker<UsuarioRequest>()
+
+                .RuleFor(r => r.nome, f => f.Person.FirstName)
+                .RuleFor(e => e.email, (f, user) => f.Internet.Email(user.nome))
+                .RuleFor(s => s.senha, f => f.Internet.Password());
+                
+        }
+    }
+}
---
+++ ValidarTestes_UseCase/TesteUseCase.cs
@@ -0,0 +1,22 @@
+﻿using ApiLogin.Application.UseCases.Usuario;
+using UseCases_Testes.Usuario;
+
+namespace ValidarTestes_UseCase
+{
+    public class TesteUseCase
+    {
+        [Fact]
+        public async Task Sucess()
+        {
+            // arrange
+            var request = UseCases_Testes.Usuario.ValidarUsuario.validate();
+            var validate = new ApiLogin.Application.UseCases.Usuario.ValidarUsuario();
+
+            // act
+            var result = validate.Validate(request);
+
+            
+            Assert.True(result.IsValid);
+        }
+    }
+}
